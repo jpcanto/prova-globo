289e8be0510a66a2bfe594088a78b792b5f82a37
jpcanto <jplopessk8@gmail.com>
2021-02-07 14:01:14 -0300

    feat: adding 404 page

---
+++ frontend/src/views/NotFound.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="notfound">
+    <v-img contain src="../assets/404.png"></v-img>
+    <v-btn x-large absolute top outlined color="purple" light @click="handleClick" class="btn ma-10">Voltar á Página inicial</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+  methods: {
+    handleClick() {
+      return this.$router.push('/');
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.v-image {
+  height: 100vh;
+  width: -webkit-fill-available;
+}
+</style>
