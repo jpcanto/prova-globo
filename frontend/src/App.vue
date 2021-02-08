f8cff21edcac16b4bca3a54bb4edce0c7b7fa448
jpcanto <jplopessk8@gmail.com>
2021-02-08 19:31:07 -0300

    style: adding striped style to rows

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,3 +11,18 @@ export default {
   name: 'App'
 };
 </script>
+
+<style lang="scss">
+.v-data-table tbody tr:nth-of-type(even) {
+  background-color: rgba(0, 0, 0, 0.034);
+}
+.v-data-table tbody tr:nth-of-type(odd) {
+  background-color: rgba(255, 255, 255, 0.5);
+}
+.v-data-table tbody tr.inactive {
+  background-color: #fff;
+  & td:not(:last-child) {
+    opacity: 0.5;
+  }
+}
+</style>
