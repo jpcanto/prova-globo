fe1a81b4aa608d3c2fb1cefb68495fca79df78dd
jpcanto <jplopessk8@gmail.com>
2021-02-09 19:42:57 -0300

    refact: changing methods order

--- backend/src/controllers/ChallengeController.js
+++ backend/src/controllers/ChallengeController.js
@@ -1,4 +1,41 @@
 class ChallengeController {
+  async one(req, res) {
+    const { array, target } = req.body;
+
+    const matchers = [];
+    const matchersIndexes = [];
+
+    for (let i = 0; i < array.length; i++) {
+      for (let j = i + 1; j < array.length; j++) {
+        if (target == array[i] + array[j]) {
+          matchers.push([array[i], array[j]]);
+          matchersIndexes.push([[i], [j]]);
+        }
+      }
+    }
+
+    if (!Array.isArray(array))
+      return res.status(400).json({
+        message: `Erro, O parâmetro 'array' precisa ser do tipo Array`,
+      });
+
+    if (typeof target !== "number")
+      return res.status(400).json({
+        message: `Erro, O parâmetro 'target' precisa ser do tipo number`,
+      });
+
+    if (matchers.length)
+      return res.status(200).json({
+        message: `Expressão válida, esses são os pares que juntos somam o alvo, ${matchers.map(
+          (m) => `[${m}]`
+        )}, e seus indexes no array: ${matchersIndexes.map((m) => `[${m}]`)}`,
+      });
+    else
+      return res.status(200).json({
+        message: `Expressão inválida, nenhum par soma o alvo fornecido`,
+      });
+  }
+
   async two(req, res) {
     const { string } = req.params;
 
@@ -32,43 +69,6 @@ class ChallengeController {
       return res.status(200).json({
         message:
           "Expressão Válida, Todos os brackets estão fechando corretamente",
-      });
-  }
-
-  async one(req, res) {
-    const { array, target } = req.body;
-
-    const matchers = [];
-    const matchersIndexes = [];
-
-    for (let i = 0; i < array.length; i++) {
-      for (let j = i + 1; j < array.length; j++) {
-        if (target == array[i] + array[j]) {
-          matchers.push([array[i], array[j]]);
-          matchersIndexes.push([[i], [j]]);
-        }
-      }
-    }
-
-    if (!Array.isArray(array))
-      return res.status(400).json({
-        message: `Erro, O parâmetro 'array' precisa ser do tipo Array`,
-      });
-
-    if (typeof target !== 'number')
-      return res.status(400).json({
-        message: `Erro, O parâmetro 'target' precisa ser do tipo number`,
-      });
-
-    if (matchers.length)
-      return res.status(200).json({
-        message: `Expressão válida, esses são os pares que juntos somam o alvo, ${matchers.map(
-          (m) => `[${m}]`
-        )}, e seus indexes no array: ${matchersIndexes.map((m) => `[${m}]`)}`,
-      });
-    else
-      return res.status(200).json({
-        message: `Expressão inválida, nenhum par soma o alvo fornecido`,
       });
   }
 }
