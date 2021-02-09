e95fcfc8d8f2662bd650612e6a32739c947e307e
jpcanto <jplopessk8@gmail.com>
2021-02-09 02:10:46 -0300

    fix: deleting wrong test

--- frontend/src/tests/Header.vue.test.js
+++ frontend/src/tests/Header.vue.test.js
@@ -53,14 +53,4 @@ describe('Home suit', () => {
     button.trigger('click');
     expect(drawer.classes()).toContain('v-navigation-drawer--fixed');
   });
-
-  it('toggle create user modal', () => {
-    const button = wrapper.find('.create-user');
-    const dialog = wrapper.find('.v-dialog');
-
-    expect(button.exists()).toBe(true);
-    expect(dialog.exists()).toBe(false);
-
-    button.trigger('click').then(() => expect(dialog.exists()).toBe(true));
-  });
 });
